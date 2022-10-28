fa6d259ea5fe4f277730df694caad43ced393b31
Liesl <lieslsheridan@gmail.com>
2022-10-28 11:07:53 -0400

    Updated with Javadoc

--- TheRavenGUI/src/application/TheRavenOG.java
+++ TheRavenGUI/src/application/TheRavenOG.java
@@ -1,11 +1,27 @@
+
+
 package application;
 
 import java.io.*;
 import java.util.*;
 import java.util.Map.Entry;
 
+
+/**
+ * The purpose of this program is to accept a file from a computer, filter out any punctuation or other symbols,
+ * place the words and how often they appear into a hashmap, and then sort, reduce, and print the words and their count
+ * @author Liesl Sheridan
+ */
 public class TheRavenOG {
 
+	/**
+	 * 
+	 * @param fileName This method is used to accept a file, remove any HTML tags,
+	 *                 backslashes, or punctuation, and places the words into a
+	 *                 hashmap as a value. It counts how often the word appears in
+	 *                 the file and uses that number as the key in the hashmap
+	 * @return heRavenHashMap
+	 */
 	public static Map<String, Integer> countTheWords(String fileName) {
 
 		HashMap<String, Integer> theRavenHashMap = new HashMap<String, Integer>();
@@ -56,8 +72,14 @@ public class TheRavenOG {
 
 	}
 
-	// this is taking in a hashmap and supposed to sort it and place it in a
-	// list.return the list
+	/**
+	 * @param mapWords this method takes in a hashmap. I create a List Map.Entry
+	 *                 and fill it with the hashmap parameter. It sorts the hashmap
+	 *                 by calling Collections.sort and places it in the
+	 *                 List Map.Entry. Returns the list
+	 *                 @return theList
+	 * 
+	 */
 	public static List<Entry<String, Integer>> sortValue(Map<String, Integer> mapWords) {
 
 		// create a new list and fill it with the key/values from the inputed map
@@ -74,6 +96,13 @@ public class TheRavenOG {
 
 	}
 
+	/**
+	 * 
+	 * @param sorted This method is for reducing the List down to 20 values. It runs
+	 *               a for loop that checks if the list is larger than 20 and
+	 *               removes that value if it is true
+	 * @return sorted
+	 */
 	public static List<Map.Entry<String, Integer>> shrinkListToTwenty(List<Map.Entry<String, Integer>> sorted) {
 		for (int i = 0; i < sorted.size(); i++) {
 			while (sorted.size() > 20) // remove all but last 20 elements
@@ -82,12 +111,22 @@ public class TheRavenOG {
 		return sorted;
 	}
 
+	/**
+	 * 
+	 * @param sorted This method is simply for printing out the sorted List into the
+	 *               console
+	 */
 	public static void printTheFinalList(List<Map.Entry<String, Integer>> sorted) {
 		for (int i = 0; i < sorted.size(); i++) {
 			System.out.println(sorted.get(i));
 		}
 	}
 
+	/**
+	 * 
+	 * @param args
+	 * Main method. This assigns the file location and calls all methods
+	 */
 	public static void main(String[] args) {
 
 		String file = "/Users/lieslnick/Documents/The Raven with html? copy.txt";
@@ -95,10 +134,14 @@ public class TheRavenOG {
 		Map<String, Integer> words = new HashMap<String, Integer>();
 		List<Map.Entry<String, Integer>> sorted = new LinkedList<Map.Entry<String, Integer>>();
 
-		words = countTheWords(file); // this loads the file into a string and outputs it as a map
-		sorted = sortValue(words); // this accepts a map and then sorts it and places it in a list
-		sorted = shrinkListToTwenty(sorted); // this accepts the sorted list and reduces it to 20 key/value pairs
-		printTheFinalList(sorted); // prints out the list
+		// this loads the file into a string and outputs it as a map
+		words = countTheWords(file);
+		// this accepts a map and then sorts it and places it in a list
+		sorted = sortValue(words);
+		// this accepts the sorted list and reduces it to 20 key/value pairs
+		sorted = shrinkListToTwenty(sorted);
+		// prints out the list
+		printTheFinalList(sorted);
 
 	}
 
